4c154a0a1521f8537d812bbfa83e6656434f681e
Trisha <dastrisha127@gmail.com>
2026-06-27 13:05:48 +0530

    components commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,15 +4,15 @@ import numpy as np
 import pandas as pd
 from pymongo import MongoClient
 from zipfile import Path
-from src.constant import *
+from src.constants import *
 from src.exception import CustomException
 from src.logger import logging
 from src.utils.main_utils import MainUtils
-from dataclass import dataclass
+from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    artifact_folder : str = os.path.join(artifact_folder)
+    artifact_folder : str = os.path.join(artifacts_folder)
     
 
 
